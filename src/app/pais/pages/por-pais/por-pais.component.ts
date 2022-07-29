8083ac34b9b2534ed02785d2a4d9bd31fa855f0c
julianarias <kjulianr41@gmail.com>
2022-07-29 15:35:31 -0500

    Seccion 8 clase 106(Service buscar paises)

--- src/app/pais/pages/por-pais/por-pais.component.ts
+++ src/app/pais/pages/por-pais/por-pais.component.ts
@@ -1,14 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { PaisService } from '../../services/pais.service';
 
 @Component({
   selector: 'app-por-pais',
   templateUrl: './por-pais.component.html'
 })
-export class PorPaisComponent implements OnInit {
+export class PorPaisComponent {
 
-  constructor() { }
+  public termino: string = ''
 
-  ngOnInit(): void {
+  constructor(private paisService: PaisService) { }
+
+  buscar(){
+    console.log(this.termino);
+    this.paisService.buscarPais(this.termino).subscribe(res =>{
+      console.log(res)
+    })
   }
-
 }
